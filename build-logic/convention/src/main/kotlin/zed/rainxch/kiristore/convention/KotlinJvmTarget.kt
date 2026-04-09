9262fbb79df8ec8fa69200679cb6656e7e2d29c5
Krishna Patil <krishnacpatil2006@gmail.com>
2026-04-09 16:29:53 +0530

    Update KotlinJvmTarget.kt

--- build-logic/convention/src/main/kotlin/zed/rainxch/kiristore/convention/KotlinJvmTarget.kt
+++ build-logic/convention/src/main/kotlin/zed/rainxch/kiristore/convention/KotlinJvmTarget.kt
@@ -6,7 +6,7 @@ import org.gradle.kotlin.dsl.getting
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
 import java.util.Properties
 
-internal fun Project.configureJvmTarget() {
+fun Project.configureJvmTarget() {
     extensions.configure<KotlinMultiplatformExtension> {
         jvm()
     }
