f6a54f6b248ea092bfbb377d20da8cc9b64ee6ea
Krishna Patil <krishnacpatil2006@gmail.com>
2026-04-09 16:29:55 +0530

    Update KotlinMultiplatform.kt

--- build-logic/convention/src/main/kotlin/zed/rainxch/kiristore/convention/KotlinMultiplatform.kt
+++ build-logic/convention/src/main/kotlin/zed/rainxch/kiristore/convention/KotlinMultiplatform.kt
@@ -5,7 +5,7 @@ import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
 
-internal fun Project.configureKotlinMultiplatform() {
+fun Project.configureKotlinMultiplatform() {
     if (!isAndroidDisabled) {
         extensions.configure<LibraryExtension> {
             namespace = this@configureKotlinMultiplatform.pathToPackageName()
