c5f9648409a098b94ad9e9e48fdc8d85bcc7153b
Krishna Patil <krishnacpatil2006@gmail.com>
2026-04-14 15:29:59 +0530

    Update AndroidCompose.kt

--- build-logic/convention/src/main/kotlin/zed/rainxch/kiristore/convention/AndroidCompose.kt
+++ build-logic/convention/src/main/kotlin/zed/rainxch/kiristore/convention/AndroidCompose.kt
@@ -4,19 +4,18 @@ import com.android.build.api.dsl.CommonExtension
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.dependencies
 
-fun Project.configureAndroidCompose(commonExtension: CommonExtension<*, *, *, *, *, *>) {
-    with(commonExtension) {
-        buildFeatures {
-            compose = true
-        }
+fun Project.configureAndroidCompose(commonExtension: CommonExtension) {
+    commonExtension.apply {
+        (this as? com.android.build.api.dsl.ApplicationExtension)?.buildFeatures?.compose = true
+        (this as? com.android.build.api.dsl.LibraryExtension)?.buildFeatures?.compose = true
 
         dependencies {
             val composeBom = libs.findLibrary("androidx-compose-bom").get()
 
-            "implementation"(platform(composeBom))
-            "testImplementation"(platform(composeBom))
-            "debugImplementation"(libs.findLibrary("androidx-compose-ui-tooling-preview").get())
-            "debugImplementation"(libs.findLibrary("androidx-compose-ui-tooling").get())
+            add("implementation", platform(composeBom))
+            add("testImplementation", platform(composeBom))
+            add("debugImplementation", libs.findLibrary("androidx-compose-ui-tooling-preview").get())
+            add("debugImplementation", libs.findLibrary("androidx-compose-ui-tooling").get())
         }
     }
 }
