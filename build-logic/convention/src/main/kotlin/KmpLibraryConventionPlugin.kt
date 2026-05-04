50edea2895d675df9258b46d922c89e43e3fd92f
Krishna Patil <krishnacpatil2006@gmail.com>
2026-05-04 21:09:54 +0530

    Update KmpLibraryConventionPlugin.kt

--- build-logic/convention/src/main/kotlin/KmpLibraryConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/KmpLibraryConventionPlugin.kt
@@ -13,10 +13,10 @@ class KmpLibraryConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         with(target) {
             with(pluginManager) {
+                apply("org.jetbrains.kotlin.multiplatform")
                 if (!isAndroidDisabled) {
                     apply("com.android.library")
                 }
-                apply("org.jetbrains.kotlin.multiplatform")
                 apply("org.jetbrains.kotlin.plugin.serialization")
             }
 
